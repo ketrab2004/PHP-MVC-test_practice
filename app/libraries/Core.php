dcb4ed929d081d53c5477519c716f9d679725c06
Bartek O <bartek@fos.nl>
2022-03-11 11:14:25 +0100

    added else in core

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -51,6 +51,8 @@ class Core {
             $url = filter_var($url, FILTER_SANITIZE_URL);
             $url = explode('/', $url);
             return $url;
+        } else {
+            return ["pages", "index"];
         }
     }
 }
